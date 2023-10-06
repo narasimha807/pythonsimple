715bdcea14bed65f9155516a71994848f9ce4d50
narasimhanaidu <talarinarasimhanaidu1@gmail.com>
2023-10-06 08:40:57 +0530

    Substracting  two number

--- sum.py
+++ sum.py
@@ -2,7 +2,9 @@ print("Hello World!")
 print("Hi to everyone")
 print("this is only practice purpose don't miss use of this application")
 
-#Adding two number 
+##Adding two number 
 a = 2
 b = 3
 print(a+b)
+#Substracting  two number 
+print(a-b)
